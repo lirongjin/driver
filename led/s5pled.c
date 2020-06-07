83bc0455b4f23b4a33a57a8a4d219160890f756f
lirongjin <lrj1826287002@gmail.com>
2020-06-08 06:17:02 +0800

    Adjust the layout

--- led/s5pled.c
+++ led/s5pled.c
@@ -12,37 +12,37 @@
 //#define CONFIG_STATIC_MAP
 #define CONFIG_DYNAMIC_MAP
 
-#define LED_NAME		"s5pled"
+#define LED_NAME        "s5pled"
 
-#define LED_GPIO_CON		(S5PV210_GPJ0CON)
-#define LED_GPIO_DAT		(S5PV210_GPJ0DAT)
+#define LED_GPIO_CON        (S5PV210_GPJ0CON)
+#define LED_GPIO_DAT        (S5PV210_GPJ0DAT)
 
-#define LED_GPIO_CON_PA		(0xe0200240)
-#define LED_GPIO_DAT_PA		(0xe0200244)
+#define LED_GPIO_CON_PA     (0xe0200240)
+#define LED_GPIO_DAT_PA     (0xe0200244)
 
 #if defined(CONFIG_STATIC_MAP)
-# define rLED_GPIO_CON		(*(volatile unsigned int *)LED_GPIO_CON)
-# define rLED_GPIO_DAT		(*(volatile unsigned int *)LED_GPIO_DAT)
+# define rLED_GPIO_CON      (*(volatile unsigned int *)LED_GPIO_CON)
+# define rLED_GPIO_DAT      (*(volatile unsigned int *)LED_GPIO_DAT)
 #elif defined(CONFIG_DYNAMIC_MAP)
-# define rLED_GPIO_CON		(*(volatile unsigned int *)led_con)
-# define rLED_GPIO_DAT		(*(volatile unsigned int *)led_dat)
+# define rLED_GPIO_CON      (*(volatile unsigned int *)led_con)
+# define rLED_GPIO_DAT      (*(volatile unsigned int *)led_dat)
 #endif
 
 /*3个led灯在GPIO_DAT寄存器中的bit位*/
-#define LED0			3
-#define LED1			4
-#define LED2			5
+#define LED0            3
+#define LED1            4
+#define LED2            5
 
 /*led打开关闭操作的宏定义和查看led状态的宏定义*/
-#define led_on(bit)			({rLED_GPIO_CON |= 1 << (bit); \
-								rLED_GPIO_DAT &= ~(1 << (bit)); \
-							})
-#define led_off(bit)		({rLED_GPIO_CON |= 1 << (bit); \
-								rLED_GPIO_DAT |= (1 << (bit)); \
-							})		
-#define led_status(bit)		({rLED_GPIO_CON &= ~(1 << (bit)); \
-								rLED_GPIO_DAT & (1 << (bit)) ? 0 : 1; \
-							})
+#define led_on(bit)         ({rLED_GPIO_CON |= 1 << (bit); \
+                                rLED_GPIO_DAT &= ~(1 << (bit)); \
+                            })
+#define led_off(bit)        ({rLED_GPIO_CON |= 1 << (bit); \
+                                rLED_GPIO_DAT |= (1 << (bit)); \
+                            })      
+#define led_status(bit)     ({rLED_GPIO_CON &= ~(1 << (bit)); \
+                                rLED_GPIO_DAT & (1 << (bit)) ? 0 : 1; \
+                            })
 
 
 /*led设备文件的打开、关闭、读和写函数的声明*/
@@ -59,11 +59,11 @@ static int led_major = 0;
 
 /*led文件操作结构体定义，非常关键!!!*/
 static const struct file_operations led_chrdev = {
-	.owner = THIS_MODULE,
-	.open = led_open,
-	.release = led_release,
-	.read = led_read,
-	.write = led_write
+    .owner = THIS_MODULE,
+    .open = led_open,
+    .release = led_release,
+    .read = led_read,
+    .write = led_write
 };
 
 
@@ -76,25 +76,25 @@ static const struct file_operations led_chrdev = {
 static int led_open(struct inode *inode, struct file *file)
 {
 #ifdef CONFIG_DYNAMIC_MAP
-	/*申请和映射操作led的IO资源*/
-	if(!request_mem_region(LED_GPIO_CON_PA, 4, "led_con") || !request_mem_region(LED_GPIO_CON_PA, 4, "led_dat"))
-	{
-		return -EBUSY;
-	}
-	
-	led_con = ioremap(LED_GPIO_CON_PA, 4);
-	if(!led_con)
-	{
-		return -EBUSY;
-	}
-	led_dat = ioremap(LED_GPIO_DAT_PA, 4);
-	if(!led_dat)
-	{
-		return -EBUSY;
-	}
+    /*申请和映射操作led的IO资源*/
+    if(!request_mem_region(LED_GPIO_CON_PA, 4, "led_con") || !request_mem_region(LED_GPIO_CON_PA, 4, "led_dat"))
+    {
+        return -EBUSY;
+    }
+    
+    led_con = ioremap(LED_GPIO_CON_PA, 4);
+    if(!led_con)
+    {
+        return -EBUSY;
+    }
+    led_dat = ioremap(LED_GPIO_DAT_PA, 4);
+    if(!led_dat)
+    {
+        return -EBUSY;
+    }
 #endif
   
-	return 0;
+    return 0;
 }
 
 /*
@@ -106,14 +106,14 @@ static int led_open(struct inode *inode, struct file *file)
 static int led_release(struct inode *inode, struct file *file)
 {
 #ifdef CONFIG_DYNAMIC_MAP
-	/*解除映射和释放led的IO资源*/
-	iounmap(led_con);
-	iounmap(led_dat);
-	release_mem_region(LED_GPIO_CON_PA, 4);
-	release_mem_region(LED_GPIO_DAT_PA, 4);
+    /*解除映射和释放led的IO资源*/
+    iounmap(led_con);
+    iounmap(led_dat);
+    release_mem_region(LED_GPIO_CON_PA, 4);
+    release_mem_region(LED_GPIO_DAT_PA, 4);
 #endif
-	
-	return 0;
+    
+    return 0;
 }
 
 /*
@@ -126,33 +126,33 @@ static int led_release(struct inode *inode, struct file *file)
  **/
 static ssize_t led_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos)
 {
-	int ret;
-	char kbuf[20] = {0};
+    int ret;
+    char kbuf[20] = {0};
 
   
-	printk(KERN_DEBUG "led_read start\n");
-	
-	if(count > sizeof(kbuf))
-	{
-		count = sizeof(kbuf);
-	}
-	
-	/*读取led当前状态并写入kbuf中，接下来把kbuf中的内容写入到用户应用层buf中。*/
-	memset(kbuf, 0, sizeof(kbuf));
-	kbuf[0] = led_status(LED0);
-	kbuf[1] = led_status(LED1);
-	kbuf[2] = led_status(LED2);
+    printk(KERN_DEBUG "led_read start\n");
+    
+    if(count > sizeof(kbuf))
+    {
+        count = sizeof(kbuf);
+    }
+    
+    /*读取led当前状态并写入kbuf中，接下来把kbuf中的内容写入到用户应用层buf中。*/
+    memset(kbuf, 0, sizeof(kbuf));
+    kbuf[0] = led_status(LED0);
+    kbuf[1] = led_status(LED1);
+    kbuf[2] = led_status(LED2);
 
-	ret = copy_to_user(ubuf, kbuf, count);
-	if(ret)
-	{
-		printk(KERN_ERR "led_read rest is %d\n", ret);
+    ret = copy_to_user(ubuf, kbuf, count);
+    if(ret)
+    {
+        printk(KERN_ERR "led_read rest is %d\n", ret);
 
-		return -1;
-	}
-	printk(KERN_DEBUG "led_read success copy\n");
+        return -1;
+    }
+    printk(KERN_DEBUG "led_read success copy\n");
 
-	return 0;
+    return 0;
 }
 
 /*
@@ -165,32 +165,32 @@ static ssize_t led_read(struct file *file, char __user *ubuf, size_t count, loff
  **/
 static ssize_t led_write(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos)
 {
-	int ret;
-	char kbuf[20] = {0};
+    int ret;
+    char kbuf[20] = {0};
 
 
-	printk(KERN_DEBUG "led_write start\n");
+    printk(KERN_DEBUG "led_write start\n");
 
-	/*读取用户应用层buf中的内容，根据默认的定义操作相应的led亮灭。*/
-	memset(kbuf, '\0', sizeof(kbuf));
-	if(count > sizeof(kbuf))
-	{
-		count = sizeof(kbuf);
-	}
-	ret = copy_from_user(kbuf, ubuf, count);
-	if(ret)
-	{
-		printk(KERN_ERR "led_write rest is %d\n", ret);
+    /*读取用户应用层buf中的内容，根据默认的定义操作相应的led亮灭。*/
+    memset(kbuf, '\0', sizeof(kbuf));
+    if(count > sizeof(kbuf))
+    {
+        count = sizeof(kbuf);
+    }
+    ret = copy_from_user(kbuf, ubuf, count);
+    if(ret)
+    {
+        printk(KERN_ERR "led_write rest is %d\n", ret);
 
-		return count - ret;
-	}
-	printk(KERN_DEBUG "led_write success content is %s\n", kbuf);
-	
-	kbuf[0] ? led_on(LED0) : led_off(LED0);
-	kbuf[1] ? led_on(LED1) : led_off(LED1);
-	kbuf[2] ? led_on(LED2) : led_off(LED2);
+        return count - ret;
+    }
+    printk(KERN_DEBUG "led_write success content is %s\n", kbuf);
+    
+    kbuf[0] ? led_on(LED0) : led_off(LED0);
+    kbuf[1] ? led_on(LED1) : led_off(LED1);
+    kbuf[2] ? led_on(LED2) : led_off(LED2);
 
-	return count;
+    return count;
 }
 
 /*
@@ -199,19 +199,19 @@ static ssize_t led_write(struct file *file, const char __user *ubuf, size_t coun
  **/
 static int __init led_init(void)
 {
-	printk(KERN_DEBUG "led_init\n");
-	
-	/*注册led file_operations结构体，返回主设备号。*/
-	led_major = register_chrdev(0, LED_NAME, &led_chrdev);
-	if(led_major < 0)
-	{
-		printk(KERN_ERR "register_led fail\n");
+    printk(KERN_DEBUG "led_init\n");
+    
+    /*注册led file_operations结构体，返回主设备号。*/
+    led_major = register_chrdev(0, LED_NAME, &led_chrdev);
+    if(led_major < 0)
+    {
+        printk(KERN_ERR "register_led fail\n");
 
-		return led_major;
-	}
-	printk(KERN_INFO "register_led success\nmajor is %d\n", led_major);
+        return led_major;
+    }
+    printk(KERN_INFO "register_led success\nmajor is %d\n", led_major);
 
-	return 0;
+    return 0;
 }
 
 /*
@@ -221,8 +221,8 @@ static int __init led_init(void)
  **/
 static void __exit led_exit(void)
 {
-	printk(KERN_DEBUG "led_exit\n");
-	unregister_chrdev(led_major, LED_NAME);
+    printk(KERN_DEBUG "led_exit\n");
+    unregister_chrdev(led_major, LED_NAME);
 }
 
 module_init(led_init);
